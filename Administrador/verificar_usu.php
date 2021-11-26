ddc97bd61fc3889c3c6417d79d1ef3017cb0d1f5
Luiz Nardes <luizgnades123@gmail.com.br>
2021-11-25 21:28:49 -0300

    25-11-2021

---
+++ Administrador/verificar_usu.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Menu</title>
+  </head>
+  <body >
+    <nav class="p-3 mb-2 bg-dark text-white navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <img class="rounded-circle" src="img/logo2.0.jpeg" alt="logo da loja">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="p-3 mb-2 bg-dark text-white nav-link active" aria-current="page" >Nome Loja</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"></a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="p-3 mb-2 bg-dark text-white nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Usuarios
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="verificar_usu.php">Verificar</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Adicionar</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Excluir</a></li>
+                </ul>
+              </li>
+
+              <li class="nav-item dropdown">
+                <a class="p-3 mb-2 bg-dark text-white nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Produtos
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Verificar</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Adicionar</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Excluir</a></li>
+                </ul>
+              </li>
+
+              <li class="nav-item dropdown">
+                <a class="p-3 mb-2 bg-dark text-white nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Mesas
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#" >Verificar</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Alterar</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Adicionar</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Excluir </a></li>
+                </ul>
+              </li>
+            </ul>
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="text-white nav-link " href="sair.php" tabindex="-1" >Sair</a>
+                  </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+<br><br><br>
+        <div class="container-fluid d-flex justify-content-center">
+            <div class="row gx-5">
+                <div class="col-3 text-center">
+                    <label for="vName">Nome</label>
+                    <input class="form-control " type="text" id="vName" placeholder="Cesar">
+                </div>
+                <div class="col-3 text-center">
+                    <label for="vEmail">Email</label>
+                    <input class="form-control " type="text" id="vEmail" placeholder="Cesar@gmail.com">
+                </div>
+                <div class="col-3 text-center">
+                    <label for="vTipo">Tipo</label>
+                    <input class="form-control " type="text" id="vTipo" placeholder="Administrador">
+                </div>
+                <div class="col-3 p-4">
+                    <button class="btn bg-dark text-white">Consultar</button>
+                </div>
+        </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
