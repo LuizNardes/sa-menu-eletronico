f6cf9121f4e66154a8952f7142705b20d63a86b8
Luiz Nardes <89425839+LuizNardes@users.noreply.github.com>
2021-12-14 07:53:51 -0300

    14/12

--- PHP/salvaProduto.php
+++ PHP/salvaProduto.php
@@ -6,15 +6,12 @@
     $desc = $_POST['nDesc'];
     $preco = $_POST['nPreco'];
     $categoria = $_POST['nCategoria'];
-
-    include("../PHP/conexao.php");
+H
+    include("conexao.php");
 
     $sql = "INSERT INTO produtos"
     ."(nome, qtd, descricao, preco, id-categoria)"
     . "VALUES ('".$nome."','".$qtd."','".$desc."',".$preco.",".$categoria.");";
-
-    var_dump($sql);
-    die();
 
     $result = mysqli_query($conn,$sql);
     mysqli_close($conn);
--- addProduto.php
+++ addProduto.php
@@ -1,18 +1,75 @@
-<?php
-    include('../php/listarCategorias.php');
-
-?>
-
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Menu</title>
+  </head>
+  <body style="height: 655px;">
+  <nav class="p-3 mb-2 bg-dark text-white navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <img class="rounded-circle" src="dist/images/logo2.0.jpeg" alt="logo da loja">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="p-3 mb-2 bg-dark text-white nav-link active" aria-current="page" >Nome Loja</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"></a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="p-3 mb-2 bg-dark text-white nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Usuarios
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="listarUser.php">Verificar</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Adicionar</a></li>              
+                </ul>
+              </li>
+
+              <li class="nav-item dropdown">
+                <a class="p-3 mb-2 bg-dark text-white nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Produtos
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="listarProduto.php">Verificar</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Adicionar</a></li>
+                </ul>
+              </li>
+
+              <li class="nav-item dropdown">
+                <a class="p-3 mb-2 bg-dark text-white nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Mesas
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#" >Verificar</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Alterar</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Adicionar</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Excluir </a></li>
+                </ul>
+              </li>
+            </ul>
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="text-white nav-link " href="php/sair.php" tabindex="-1" >Sair</a>
+                  </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+<br><br><br>
 <form method="POST" action="php/functionProdutos.php">
         <div>
             <label for="iNome">Nome: </label>
@@ -35,7 +92,7 @@
         <div>
             <label for="iCategoria">Categoria: </label>
             <select name="nCategoria" id="iCategorias">
-                <?php echo listar_categorias();  ?>
+                <?php echo listarCategorias();  ?>
           </select>
         </div>
         <p>
@@ -47,5 +104,13 @@
             <button type="submit" name="button">Cadastrar</button>
         </div>
     </form>
+      <footer class="container-fluid d-flex flex-wrap justify-content-center align-items-center py-3 my-4 border-top" style="position:fixed;bottom:0;">
+    <div class="col-md-2 d-flex align-items-center">
+      <span class="text-center fw-bold ">© 2021 projetinho </span>
+    </div>
+  </footer>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
